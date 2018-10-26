b1db259577f7ce367f81320cdb22ff8f020c721b
Casper Rogild Storm <casper+github@rogildstorm.com>
2018-10-26 15:49:33 +0200

    updated stories a bit

--- src/components/upload-demo/index.stories.tsx
+++ src/components/upload-demo/index.stories.tsx
@@ -3,5 +3,10 @@ import Upload from './index';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf("Upload", module)
-  .add('Default', () => <Upload text={'Lorem ipsum dolor sit amet'} hint={'Fusce malesuada magna leo, quis pharetra urna finibus nec. Sed vitae ex mauris. Mauris orci ex.'}/>);
+storiesOf("upload-demo", module)
+  .add('default', () => 
+  <Upload 
+    text={'Lorem ipsum dolor sit amet'} 
+    hint={'Fusce malesuada magna leo, quis pharetra urna finibus nec. Sed vitae ex mauris. Mauris orci ex.'}
+  />
+);

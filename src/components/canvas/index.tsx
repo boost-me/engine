8071731b2c233587890f711cee831d58a0e15d8f
Casper Rogild Storm <casper+github@rogildstorm.com>
2018-11-01 14:14:44 +0100

    now compiles and displays a base image

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -1,11 +1,20 @@
-import { Sprite, Stage } from '@inlet/react-pixi'
 import * as React from 'react'
+import { Sprite, Stage } from 'react-pixi-fiber'
+import mirage from '../../../assets/maps/de_mirage.jpg'
 
 export class Canvas extends React.PureComponent<{}> {
+  public height = 800
+  public width = 800
+  public OPTIONS = {}
+
   public render() {
     return (
-      <Stage>
-        <Sprite image="./bunny.png" x={100} y={100} />
+      <Stage width={this.width} height={this.height} options={this.OPTIONS}>
+        <Sprite
+          width={this.width}
+          height={this.height}
+          texture={PIXI.Texture.fromImage(mirage)}
+        />
       </Stage>
     )
   }

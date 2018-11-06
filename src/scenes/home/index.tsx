1ee0aed0b85717afa3dfa992afda007f61d9e27e
Casper Rogild Storm <casper+github@rogildstorm.com>
2018-11-06 14:05:07 +0100

    added draft for homescene

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,33 +1,60 @@
-import { Button, Popover } from 'antd'
+import { Icon, Layout, Menu } from 'antd'
 import * as React from 'react'
 import './index.css'
 
+const { Header, Sider, Content } = Layout
 class HomeScene extends React.Component {
   public state = {
-    visible: false,
+    collapsed: false,
   }
 
-  public hide = () => {
+  public toggle = () => {
     this.setState({
-      visible: false,
+      collapsed: !this.state.collapsed,
     })
-  }
-
-  public handleVisibleChange = (visible: boolean) => {
-    this.setState({ visible })
   }
 
   public render() {
     return (
-      <div className="App">
-        <Popover
-          content={<a onClick={this.hide}>hello world :)</a>}
-          trigger="click"
-          visible={this.state.visible}
-          onVisibleChange={this.handleVisibleChange}>
-          <Button type="primary">BOOST ME/ENGINE</Button>
-        </Popover>
-      </div>
+      <Layout>
+        <Sider
+          theme="dark"
+          style={{
+            height: '100vh',
+          }}
+          trigger={null}
+          collapsible={true}
+          collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="video-camera" />
+              <span>View</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="upload" />
+              <span>Upload</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '0px 0px',
+              padding: 24,
+              background: '#fff',
+            }}>
+            content
+          </Content>
+        </Layout>
+      </Layout>
     )
   }
 }
